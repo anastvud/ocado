098967d17a46c22cdf58f1707a138c01b1f08640
anastvud <anastasiiavudvud@gmail.com>
2024-04-06 13:34:07 +0200

    init2

---
+++ src/main/java/com/ocado/basket/BasketSplitter.java
@@ -0,0 +1,11 @@
+package com.ocado.basket;
+import com.google.gson.Gson;
+
+
+public class BasketSplitter {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+
+        System.out.println("Hello world!");
+    }
+}
